1e4cfa835f4c6c2557135c9dbcff886a35f03ee8
Robson Barbosa <robson212.212@gmail.com>
2019-08-16 20:20:34 -0300

    Add files via upload

---
+++ src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculadoraService {
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+  constructor() { }
+
+  calcular(n1: number, n2: number, operacao: string): number {
+  	let resultado: number;
+
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = n1 + n2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = n1 - n2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = n1 / n2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = n1 * n2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
+  }
+}
